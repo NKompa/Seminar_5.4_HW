1cc6910ea4f1f9e7cfb6b888bce3e73b52cd9e32
NKompa <nkompa7@gmail.com>
2022-10-25 15:39:24 +0300

    Find number with needed accuracy

---
+++ 01_Number_accuracy.py
@@ -0,0 +1,15 @@
+# Вычислить число c заданной точностью d: при d = 0.001, π = 3.141
+
+a = int(input('Введите делимое: '))
+b = int(input('Введите делитель: '))
+d = int(input('Введите точность (кол-во знаков после запятой): '))
+print(a/b)                  # для проверки
+number1 = a//b
+c = number1
+count = 0
+for i in range(1,d+1):
+    a = (a-b*c)*10
+    c = a//b
+    number2 = c*10**-i
+    number1 = number1+number2
+print(round(number1,d))
