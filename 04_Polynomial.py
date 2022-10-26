45bdf8bd8003b06a4160dc0eb8cbc8a3902bbc1b
NKompa <nkompa7@gmail.com>
2022-10-26 18:13:32 +0300

    Updated

--- 04_Polynomial.py
+++ 04_Polynomial.py
@@ -5,8 +5,11 @@ import random
 k = int(input('Введите число от 1 до 9: '))
 list = [0]*(k+1)
 for i in range(len(list)):
-    list[i] = str(random.randint(0,101)) +'x^'+str(k) +'+'
-    if k == 0: list[i] = str(random.randint(0,101))+'=0'
+    list[i] = str(random.randint(0,101)) +'x^'+str(k) +' + '
+    if k == 1:
+        list[i] = str(random.randint(0,101))+'x + '
+    if k == 0:
+        list[i] = str(random.randint(0,101))+' = 0'
     k-=1
 print(''.join(list))
 
